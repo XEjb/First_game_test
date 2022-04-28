8d9042bb6a41b7c1aa5b0c19fd39be4cc820069b
Владислав Артамоненко <54907042+XEjb@users.noreply.github.com>
2022-04-28 22:18:31 +0300

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -3,7 +3,7 @@ from tkinter import *
 
 class Main(Frame):
     def __init__(self, root):
-        super(Main, self).__init__(root)
+        super(Main, self).__init__(root
         self.build()
 
     def build(self):

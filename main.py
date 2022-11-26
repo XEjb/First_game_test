79497e0f3a85508d56dd7b5bfbf2435eca61e7d7
Xejb <vladiq-artamonenko@rambler.ru>
2022-11-26 17:25:55 +0300

    url_for

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
@@ -7,13 +7,24 @@ menu = ['Установка', 'Приложение', 'Фидбэк']
 
 @app.route('/')
 def index():
+    print(url_for('index'))
     return render_template('index.html', menu=menu)
 
 
 @app.route("/about")
 def about():
+    print(url_for('about'))
     return render_template('about.html', title="О сайте", menu=menu)
 
+@app.route('/profile/<username>')
+def profile(username, path):
+    return f'Пользователь: {username}, {path}'
 
-if __name__ == '__main__':
-    app.run(debug=True)
+with app.test_request_context():
+    print(url_for('index'))
+    print(url_for('about'))
+    print(url_for('profile', username='xejb'))
+
+
+# if __name__ == '__main__':
+#     app.run(debug=True)

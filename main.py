b745ad4e32956a8d141ba8afab387effbc44b468
Xejb <vladiq-artamonenko@rambler.ru>
2022-11-29 23:14:06 +0300

    Adding and displaying articles from the database

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, url_for, request, flash, session, redirect, abort, g
 import sqlite3
 import os
+from FDataBase import FDataBase
 
 DATABASE = '/tmp/totoro.db'
 DEBUG = True
@@ -32,68 +33,47 @@ def get_db():
         return g.link_db
 
 
-@app.route("/")
-def index():
-    db = get_db()
-    return render_template('index.html', menu= [])
-
-
 @app.teardown_appcontext
 def close_db(error):
     if hasattr(g, 'link_db'):
         g.link_db.close()
 
-# menu = [{"name": "Установка", "url": "install-flask"},
-#         {"name": "Первое приложение", "url": "first-app"},
-#         {"name": "Фидбэк", "url": "contact"}]
-#
-#
-# @app.route('/')
-# def index():
-#     print(url_for('index'))
-#     return render_template('index.html', menu=menu)
-#
-#
-# @app.route("/about")
-# def about():
-#     print(url_for('about'))
-#     return render_template('about.html', title="О сайте", menu=menu)
-#
-#
-# @app.route("/contact", methods=['POST', 'GET'])
-# def contact():
-#     if request.method == 'POST':
-#         if len(request.form['username']) > 2:
-#             flash('Сообщение отправлено', category='success')
-#         else:
-#             flash('Ошибка отправки', category='error')
-#
-#     return render_template('contact.html', title="Фидбэк", menu=menu)
-#
-#
-# @app.route("/profile/<username>")
-# def profile(username):
-#     if 'userLogged' not in session or session['userLogged'] != username:
-#         abort(401)
-#
-#     return f"Профиль пользователя: {username}"
-#
-#
-# @app.route("/login", methods=["POST", "GET"])
-# def login():
-#     if 'userLogged' in session:
-#         return redirect(url_for('profile', username=session['userLogged']))
-#     elif request.method == 'POST' and request.form['username'] == "Nauty" and request.form['psw'] == "123":
-#         session['userLogged'] = request.form['username']
-#         return redirect(url_for('profile', username=session['userLogged']))
-#
-#     return render_template('login.html', title="Авторизация", menu=menu)
-#
-#
-# @app.errorhandler(404)
-# def pageNotFount(error):
-#     return render_template('page404.html', title='Страница не найдена', menu=menu)
-#
-#
+
+@app.route("/")
+def index():
+    db = get_db()
+    dbbase = FDataBase(db)
+    return render_template('index.html', menu=dbbase.getMenu(), posts=dbbase.getPostsAnonce())
+
+
+@app.route("/add_post", methods=["POST", "GET"])
+def addPost():
+    db = get_db()
+    dbase = FDataBase(db)
+
+    if request.method == "POST":
+        if len(request.form['name']) > 4 and len(request.form['post']) > 10:
+            res = dbase.addPost(request.form['name'], request.form['post'])
+            if not res:
+                flash('Ошибка добавления статьи', category='error')
+            else:
+                flash('Статья добавлена успешно', category='success')
+        else:
+            flash('Ошибка добавления статьи', category='error')
+
+    return render_template('add_post.html', menu=dbase.getMenu(), title="Добавление статьи")
+
+
+@app.route('/post/<int:id_post>')
+def showPost(id_post):
+    db = get_db()
+    dbase = FDataBase(db)
+    title, post = dbase.getPost(id_post)
+    if not title:
+        abort(404)
+
+    return render_template('post.html', menu=dbase.getMenu(), title=title, post=post)
+
+
 if __name__ == '__main__':
     app.run(debug=True)

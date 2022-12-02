03e7d62b9b27d3950d90e5657423b537a000b19d
Xejb <vladiq-artamonenko@rambler.ru>
2022-12-02 18:22:53 +0300

    Improving the authorization process

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import sqlite3
 import os
 from FDataBase import FDataBase
 from werkzeug.security import generate_password_hash, check_password_hash
-from flask_login import LoginManager, login_user, login_required
+from flask_login import LoginManager, login_user, login_required, current_user, logout_user
 from UserLogin import UserLogin
 
 DATABASE = '/tmp/totoro.db'
@@ -16,6 +16,9 @@ app.config.from_object(__name__)
 app.config.update(dict(DATABASE=os.path.join(app.root_path, 'totoro.db')))
 
 login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+login_manager.login_message = 'Пожалуйста авторизуйтесь для доступа к закрытым страницам'
+login_manager.login_message_category = 'success'
 
 
 @login_manager.user_loader
@@ -92,12 +95,16 @@ def showPost(alias):
 
 @app.route('/login', methods=['POST', 'GET'])
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('profile'))
+
     if request.method == 'POST':
         user = dbase.getUserByEmail(request.form['email'])
         if user and check_password_hash(user['psw'], request.form['psw']):
             userlogin = UserLogin().create(user)
-            login_user(userlogin)
-            return redirect(url_for('index'))
+            rm = True if request.form.get('remainme') else False  # запоминание пользователя
+            login_user(userlogin, remember=rm)
+            return redirect(request.args.get('next') or url_for('profile'))
 
         flash('Неверная пара логин/пароль', 'error')
 
@@ -120,6 +127,21 @@ def register():
             flash("Неверно заполнены поля", "error")
 
     return render_template("register.html", menu=dbase.getMenu(), title="Регистрация")
+
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    flash('Вы вышли из аккаунта', 'success')
+    return redirect(url_for('login'))
+
+
+@app.route('/profile')
+@login_required
+def profile():
+    return f"""<p><a href="{url_for('logout')}">Выйти из профиля</a>
+            <p>user info: {current_user.get_id()}"""
 
 
 if __name__ == '__main__':
